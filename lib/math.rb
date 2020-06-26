47584dfe79ed208d32a4d87bba755636f713d68b
Jina Strickland <jina30@gmail.com>
2020-06-26 01:41:22 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+  return num1 + num2
 end
 
 def subtraction(num1, num2)
-
+  return num1 - num2
 end
 
 def division(num1, num2)
-
+  return num1 / num2
 end
 
 def multiplication(num1, num2)
-
+  return num1 * num2
 end
 
 def modulo(num1, num2)
-
+ return num1 % num2
 end
 
 def square_root(num)
-
+  return Math.sqrt(num) 
 end
+
+
+#addition(2, 4)
+#subtraction(2, 4)
+#square_root(2)
